6af570f91780dc92db44decea57cba0852d249d7
Huynh Duc <tanghuynh09@gmail.com>
2023-02-04 15:03:31 +0700

    Tao dich vu chay Server

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const questRouter= require("./app/routes/quest.route")
+const ApiError = require("./app/api-error")
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.use("/api/quests",questRouter)
+
+app.use((req,res,next)=>{
+    return next(new ApiError(404,"Resource not founde"))
+});
+app.use((error,req,res,next)=>{
+
+    return res.status(error.statusCode || 500 ).json({
+        message:error.message || "Inernal Server Error",
+    })
+});
+module.exports = app;
